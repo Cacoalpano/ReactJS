e89e3c2642beeaa342da347632be9be185348b9b
Nguyễn Công Hưng <nambadaottbn@gmail.com>
2020-08-31 16:04:17 +0700

    Add code

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Nav, Navbar } from "react-bootstrap";
+import "./footer.css";
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer className="footer">
+        <div className="container">This is footer component</div>
+      </footer>
+    );
+  }
+}
---
+++ src/components/main/main.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Main extends Component {
+    render() {
+        return (
+            <div style = {{marginTop: "60px"}}>
+                This is main component
+            </div>
+        )
+    }
+}
